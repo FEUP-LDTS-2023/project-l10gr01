87a1da546cf94426d87dbd9e3e4bb6445d96404e
Leonardo Garcia <leonardorsg@hotmail.com>
2023-12-06 15:10:41 -0300

    implemented the singleton pattern on game class

--- src/main/java/com/ldts/steven/Game.java
+++ src/main/java/com/ldts/steven/Game.java
@@ -14,43 +14,50 @@ import java.net.URISyntaxException;
 import java.util.Scanner;
 
 public class Game {
-        private final LanternaGUI gui;
-        private State state;
 
-        public Game() throws FontFormatException, IOException, URISyntaxException {
-            this.gui = new LanternaGUI(200, 100);
-            this.state = new MenuState(new Menu());
+    private static Game instance;
+    private LanternaGUI gui;
+    private State state;
+
+    private Game() {}
+    public static Game getGame() throws FontFormatException, IOException, URISyntaxException {
+        if(instance==null) {
+            instance = new Game();
+            instance.gui = new LanternaGUI(200, 100);
+            instance.state = new MenuState(new Menu());
         }
-
-        public static void main(String[] args) throws IOException, FontFormatException, URISyntaxException {
-            new Game().start();
-
-        }
-
-        public void setState(State state) {
-            this.state = state;
-        }
-
-        private void start() throws IOException {
-            int FPS = 50;
-            int frameTime = 1000 / FPS;
-
-            while (this.state != null) {
-                long startTime = System.currentTimeMillis();
-
-                state.step(this,gui,startTime);
-
-                long elapsedTime = System.currentTimeMillis() - startTime;
-                long sleepTime = frameTime - elapsedTime;
-
-                try {
-                    if (sleepTime > 0) Thread.sleep(sleepTime);
-                } catch (InterruptedException e) {
-                }
-            }
-
-            gui.close();
-
-        }
+        return instance;
     }
+
+    public static void main(String[] args) throws IOException, FontFormatException, URISyntaxException {
+        getGame().start();
+
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    private void start() throws IOException {
+        int FPS = 50;
+        int frameTime = 1000 / FPS;
+
+        while (this.state != null) {
+            long startTime = System.currentTimeMillis();
+
+            state.step(this,gui,startTime);
+
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            long sleepTime = frameTime - elapsedTime;
+
+            try {
+                if (sleepTime > 0) Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+            }
+        }
+
+        gui.close();
+
+    }
+}
 
