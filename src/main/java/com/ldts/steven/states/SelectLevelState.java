d3047905eb64bfa3545b45b3d7fbdaaadb2b4908
Leonardo Garcia <leonardorsg@hotmail.com>
2023-12-08 18:49:10 -0300

    select level state implemented, now the user can select the desired level. Two levels only, one still implementing

---
+++ src/main/java/com/ldts/steven/states/SelectLevelState.java
@@ -0,0 +1,30 @@
+package com.ldts.steven.states;
+
+import com.ldts.steven.controller.Controller;
+import com.ldts.steven.controller.menu.MenuController;
+import com.ldts.steven.controller.selectLevel.SelectLevelController;
+import com.ldts.steven.model.menu.Menu;
+import com.ldts.steven.model.selectLevel.SelectLevel;
+import com.ldts.steven.viewer.Viewer;
+import com.ldts.steven.viewer.menu.MenuViewer;
+import com.ldts.steven.viewer.selectLevel.SelectLevelViewer;
+
+public class SelectLevelState extends State<SelectLevel>{
+
+    public SelectLevelState(SelectLevel selectLevel) {
+        super(selectLevel);
+
+    }
+
+    @Override
+    protected Controller<SelectLevel> getController() {
+        return new SelectLevelController(getModel());
+    }
+
+
+    @Override
+    protected Viewer<SelectLevel> getViewer() {
+        return new SelectLevelViewer(getModel());
+    }
+
+}
